6a724d1081e9730bfcb7a28aa08fe9da74c1a0c4
Dhruv041107 <159522576+Dhruv041107@users.noreply.github.com>
2025-09-19 16:03:15 +0530

    Time: 3 ms (89.53%), Space: 16.7 MB (11.87%) - LeetHub

---
+++ 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public: 
+    int solveOptimal(vector<int>&arr,vector<int>&lis){
+        if(arr.size()==0)   return 0;
+
+        vector<int>ans;
+        ans.push_back(arr[0]);
+        lis.push_back(1);
+
+        for(int i=1;i<arr.size();i++){
+            if(arr[i]>ans.back()){
+                ans.push_back(arr[i]);
+                lis.push_back(ans.size());
+            }
+            else{
+                auto it=lower_bound(ans.begin(),ans.end(),arr[i]);
+                int index= it - ans.begin();
+                ans[index]=arr[i];
+                lis.push_back(index+1);
+            }
+        }
+        return ans.size();
+    }
+
+    int minimumMountainRemovals(vector<int>& nums) {
+        vector<int>lis,lds;
+        
+        solveOptimal(nums,lis);
+        reverse(nums.begin(),nums.end());
+        solveOptimal(nums,lds);
+        int largestMountain=INT_MIN;
+        for(int i=0;i<nums.size();i++){
+            if(lis[i]==1 || lds[nums.size()-i-1] ==1)  continue;
+            largestMountain=max(largestMountain,lis[i]+lds[nums.size()-i-1]-1);
+        }
+        return nums.size()-largestMountain;
+
+
+    }
+};
