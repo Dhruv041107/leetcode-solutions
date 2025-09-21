e3da4c4c9a35e0fe2109d3fe4b13c56ea1d60da3
Dhruv041107 <159522576+Dhruv041107@users.noreply.github.com>
2025-09-21 12:07:27 +0530

    Time: 22 ms (81.57%), Space: 9.7 MB (95.18%) - LeetHub

---
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int solveTab(string &a, string &b) {
+        vector<int>curr(a.size()+1,0);
+        vector<int>next(b.size()+1,0);
+        for(int i=a.size()-1;i>=0;i--){
+            for(int j=b.size()-1;j>=0;j--){
+                int ans=0;
+                if (a[i] == b[j]) {
+                    ans= 1 + next[j+1]; 
+                }
+                else {
+                    ans= max(next[j], curr[j+1]); 
+                }
+                curr[j]=ans;
+            }
+            next=curr;
+        }
+       return next[0];
+    }
+    int longestPalindromeSubseq(string s) {
+        string rev=s;
+        reverse(rev.begin(),rev.end());
+        return solveTab(s,rev);
+    }
+    int minInsertions(string s) {
+        int ans=longestPalindromeSubseq(s);
+        return s.size()-ans;
+    }
+};
