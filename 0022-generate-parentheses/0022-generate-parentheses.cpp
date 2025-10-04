606e8095875df388027df565b9951ae6f32ad90c
Dhruv041107 <159522576+Dhruv041107@users.noreply.github.com>
2025-10-04 15:03:29 +0530

    Medium 10-04-2025

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void solve(vector<string>&ans,string &out,int open ,int close,int n){
+        if(open+close==2*n){
+            ans.push_back(out);
+            return;
+        }
+        // if(open<n)  solve(ans,out+"(",open+1,close,n);
+        // if(close<open)  solve(ans,out+")",open,close+1,n);  
+        /////OR
+        if(open<n){
+            out.push_back('(');
+            solve(ans,out,open+1,close,n);
+            out.pop_back();
+        }
+        if(close<open){
+            out.push_back(')');
+            solve(ans,out,open,close+1,n);
+            out.pop_back();
+        }
+    }
+    vector<string> generateParenthesis(int n) {
+        vector<string> ans;
+        string out="";
+        solve(ans,out,0,0,n);
+        return ans;
+    }
+};
