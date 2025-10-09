2028c4292a34026400df412a48e144c21cc4a7b5
Dhruv041107 <159522576+Dhruv041107@users.noreply.github.com>
2025-10-09 14:26:49 +0530

    Medium 10-09-2025

---
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxScore(vector<int>& cardPoints, int k) {
+        if(k==cardPoints.size())    return accumulate(cardPoints.begin(),cardPoints.end(),0);
+
+        int lsum=0;
+        int rsum=0;
+        int maxSum=0;
+
+        for(int i=0;i<k;i++){
+            lsum+=cardPoints[i];
+        }
+        maxSum=lsum;
+        int rindex=cardPoints.size()-1;
+        for(int i=k-1;i>=0;i--){
+            lsum-=cardPoints[i];
+            rsum+=cardPoints[rindex];
+            rindex--;
+            maxSum=max(maxSum,lsum+rsum);
+        }
+        return maxSum;
+    }
+};
