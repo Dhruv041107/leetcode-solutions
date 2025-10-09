96124823e22456f2c8c3ee1b7f7ff71933b4f013
Dhruv041107 <159522576+Dhruv041107@users.noreply.github.com>
2025-10-09 13:43:14 +0530

    Medium 10-09-2025

---
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int atmost(vector<int>&nums,int k){
+        int l=0,res=0;
+        for(int r=0;r<nums.size();r++){
+            if(nums[r]&1)   k--;
+            while(k<0){
+                if(nums[l]&1)   k++;
+                l++;
+            }
+            res+=(r-l+1);
+        }
+        return res;
+    }
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        return atmost(nums,k)-atmost(nums,k-1);
+    }
+};
