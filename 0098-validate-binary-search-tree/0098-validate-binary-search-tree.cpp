5cb408d7d60a76ac0d5f2a5156168443f8cbc7d4
Dhruv041107 <159522576+Dhruv041107@users.noreply.github.com>
2025-10-15 12:08:57 +0530

    Medium 10-15-2025

---
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool validate(TreeNode*root,long long lb,long long ub){
+        if(!root)   return true;
+
+        return root->val>lb && root->val<ub &&
+                validate(root->left,lb,root->val) &&
+                validate(root->right,root->val,ub); 
+    }
+    bool isValidBST(TreeNode* root) {
+        long long lb=LONG_MIN;
+        long long ub=LONG_MAX;
+
+        return validate(root,lb,ub);
+
+        
+    }
+};
