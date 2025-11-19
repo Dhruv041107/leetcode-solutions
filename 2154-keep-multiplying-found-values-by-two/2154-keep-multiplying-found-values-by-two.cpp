a8778bcfa74f0ad3a5da6d3b8108461eb2be0a2c
Dhruv SIngla <159522576+Dhruv041107@users.noreply.github.com>
2025-11-19 13:59:47 +0530

    Easy 11-19-2025

---
+++ 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int findFinalValue(vector<int>& nums, int original) {
+        unordered_set<int> st(nums.begin(), nums.end());
+        while (st.count(original)) original *= 2;
+        return original;
+    }
+};
+
